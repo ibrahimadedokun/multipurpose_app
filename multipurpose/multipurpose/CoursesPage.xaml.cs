c253f1f038392d3a8ae8d632f7cc7e64f5c34c36
Ibrahim Adedokun <N0828199@my.ntu.ac.uk>
2020-04-08 19:48:09 +0100

    Add project files.

---
+++ multipurpose/multipurpose/CoursesPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace multipurpose
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CoursesPage : ContentPage
+    {
+        public CoursesPage()
+        {
+            InitializeComponent();
+
+            stemImage.Source = ImageSource.FromResource("multipurpose.Images.stem_mockup.jpg");
+            progImage.Source = ImageSource.FromResource("multipurpose.Images.programming.jpg");
+            irkImage.Source = ImageSource.FromResource("multipurpose.Images.islam.jpg");
+        }
+
+        async private void irkParentGrid_Tapped(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page());
+        }
+
+        async private void progParentGrid_Tapped(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page());
+        }
+
+        async private void stemGrid_Tapped(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ScienceCoursesPage());
+        }
+    }
+}
---
+++ multipurpose/multipurpose/ScienceCoursesPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace multipurpose
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ScienceCoursesPage : ContentPage
+    {
+        public ScienceCoursesPage()
+        {
+            InitializeComponent();
+
+            eeImage.Source = ImageSource.FromResource("multipurpose.Images.ee.jpg");
+            bioImage.Source = ImageSource.FromResource("multipurpose.Images.biology.png");
+            chemImage.Source = ImageSource.FromResource("multipurpose.Images.chemistry_logo.jpg");
+        }
+
+        private void eeGrid_Tapped(object sender, EventArgs e)
+        {
+            DisplayAlert("Task", "You clicked me ", "All right");
+        }
+
+        private void bioGrid_Tapped(object sender, EventArgs e)
+        {
+            DisplayAlert("Error", "Not updated", "OK");
+        }
+
+        private void chemGrid_Tapped(object sender, EventArgs e)
+        {
+            DisplayAlert("Error", "Yeah", "Bye");
+        }
+
+        private void phyGrid_Tapped(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
